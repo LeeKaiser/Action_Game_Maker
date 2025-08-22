e6d9c54cc058e0f046542171d5d5e653fde00351
Kaiser Lee <kaiserkaiserlee@gmail.com>
2025-08-21 22:32:45 -0700

    miscilanious changes to prepare for upcoming change
    fixed dummy
    added attack test (used to test affect on other entity)

---
+++ Assets/TestAsset/AttackTest.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+using System.Collections;
+
+public class AttackTest : Ability
+{
+    
+
+    protected override IEnumerator Execute()
+    {
+        if (currentCharge > 0)
+        {
+            Debug.Log("Ability tapped or held. apply speed boost");
+            abilityStat.onAbilityUsePlayerEvent.DetectPlayerEvent();
+            currentCharge -= 1;
+        }
+        InterruptReload();
+        yield return new WaitForSeconds(abilityStat.useTime);
+    }
+
+    protected override IEnumerator ExecuteReleased(float chargeRatio)
+    {
+        Debug.Log($"Ability released with charge: {chargeRatio:F2}");
+        currentCharge -= 1;
+        yield return null;
+    }
+
+    void Update()
+    {
+        if (currentCharge < abilityStat.maxCharge && !isActive)
+        {
+            rechargeInProgress = true;
+        }
+        RecoverChargePoint(Time.deltaTime); //recharge every tick if possible
+    }
+}
